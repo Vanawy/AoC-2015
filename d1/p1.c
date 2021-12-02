1de03a310f4ac919c853495a08616257c31b68a9
Vanawy <saharchuk999@gmail.com>
2021-12-02 22:25:38 +0300

    d1p1

--- d1/p1.c
+++ d1/p1.c
@@ -10,8 +10,8 @@ int main (int argc, char *argv[])
     FILE *fp;
     fp = fopen(argv[1], "r");
     if (fp == NULL) {
-      perror("Failed");
-      return 2;
+        perror("Failed");
+        return 2;
     }
 
     int floor = 0;
